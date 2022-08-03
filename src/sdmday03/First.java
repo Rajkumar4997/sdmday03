02faa5ed27aa180837ff8d61f62c58267eb8b3c2
rajkumar <rajkumarjangamshetti22@gmail.com>
2022-08-03 12:55:45 +1200

    done

--- src/sdmday03/First.java
+++ src/sdmday03/First.java
@@ -5,6 +5,7 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("hello, welcome to git repository");
 		System.out.println("keep practicing");
+		System.out.println("keep growing");
 	}
 
 }
